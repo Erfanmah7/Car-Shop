3938ea5ccfd72717c2f2658a6cc11ab082d434ff
ofoghkca <ofoghkavirardakan@gmail.com>
2024-10-27 11:10:29 +0330

    layout

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,9 +1,22 @@
+import Link from "next/link";
+import styles from "./Layout.module.css";
+
 function Layout({ children }) {
   return (
     <>
-      <h1>Header</h1>
-      {children}
-      <h1>Footer</h1>
+      <header className={styles.header}>
+        <Link href="/">
+          <h2>CARSHOP</h2>
+          <p>Choose and Buy your car</p>
+        </Link>
+      </header>
+      <div className={styles.container}>{children}</div>
+      <footer className={styles.footer}>
+        <a href="https://erfandevart.ir" target="_blank">
+          erfandevart -
+        </a>
+        Next.js courses | ShopCar Project &copy;
+      </footer>
     </>
   );
 }
