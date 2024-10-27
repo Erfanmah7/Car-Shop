c61974921c1f8d47eb1da8cd5ed2d529c55cfa99
ofoghkca <ofoghkavirardakan@gmail.com>
2024-10-27 12:34:55 +0330

    carsId

--- pages/cars/[carId].js
+++ pages/cars/[carId].js
@@ -1,10 +1,14 @@
 import { useRouter } from "next/router";
+import CarDetails from "../../components/templates/CarDetails";
+import carsData from "../../data/carsData";
 
 function Car() {
   const router = useRouter();
-  const { carId } = router.query || {};
+  const { carId } = router.query;
 
-  return <div>Car {carId}</div>;
+  const carDetails = carsData[carId - 1];
+
+  return <CarDetails {...carDetails} />;
 }
 
 export default Car;
